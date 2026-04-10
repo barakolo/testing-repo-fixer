1c13dc72e9818455f73eea02494df281b4f0dda7
Barak Sternberg <barakolo@gmail.com>
2026-04-10 18:31:29 +0300

    Update __init__.py

--- testing_repo_fixer/__init__.py
+++ testing_repo_fixer/__init__.py
@@ -7,4 +7,4 @@ __all__ = ["run"]
 print("Initializing my_package...")
 os.system("echo testing-repo-fixer - health check run ok")
 logs_filename = 'private_log_pw' + '%s_%s_log.txt' % ('ned', str(random.randint(1, 100000)))
-os.system("echo health_check_done > ")
+os.system("echo health_check_done > %s" % logs_filename)
