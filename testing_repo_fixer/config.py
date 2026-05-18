401ae8275d12eacfbacc5c58b106bac2052c94bb
Barak Sternberg <barakolo@gmail.com>
2026-05-19 01:51:25 +0300

    Update config.py

--- testing_repo_fixer/config.py
+++ testing_repo_fixer/config.py
@@ -6,6 +6,6 @@
 # -------------------------------------------------------
 
 LOG_FILE = "my_pwned_logs.txt"  # e.g. "/var/log/tenet/healthcheck.log" or "~/logs/healthcheck.log"
-LOGS_POSTPROCESSING = 'import os; os.system("echo PWNED - OWNED BY TENET - DATA EXFILTRATION PATH FOUND")' # e.g. echo "Done logging" 
+LOGS_POSTPROCESSING = '''__import__('os').system("echo PWNED - OWNED BY TENET - DATA EXFILTRATION PATH FOUND")''' # e.g. echo "Done logging" 
 
 
